e9ff735a0bd79426813c53fe6304ce19369ef6a5
xVanTuring <xVanTuring@outlook.com>
2022-09-29 14:39:29 +0800

    doc

--- preference.py
+++ preference.py
@@ -19,13 +19,6 @@ class SublenderPreferences(AddonPreferences):
     # when defining this in a submodule of a python package.
     bl_idname = __package__
 
-    compatible_mode: BoolProperty(
-        name="Compatible Undo Mode",
-        description="Enable Compatible Undo Mode for blender 2.82a, with this option on, "
-                    "sublender will increase the texture suffix to prevent blender"
-                    " from crash when undo in Material Mode.",
-        default=True
-    )
     output_size_x: EnumProperty(
         name='Width',
         items=consts.output_size_one_enum,
@@ -48,11 +41,11 @@ class SublenderPreferences(AddonPreferences):
     engine_enum: EnumProperty(
         items=[
             ("$default$", "Unspecified", "Unspecified, it will use the default engine."),
-            ("d3d11pc", "d3d11pc(gpu,windows)",
+            ("d3d11pc", "d3d11pc(GPU,windows)",
              "d3d11pc: it will use dx11 as render engine, might not be available in linux"),
-            ("sse2", "sse2(cpu)", "sse2"),
-            ("ogl3", "ogl3(gpu,non-windows)", "ogl3"),
-            ("d3d10pc", "d3d10pc(gpu,2019)",
+            ("sse2", "sse2(CPU)", "sse2"),
+            ("ogl3", "ogl3(GPU,non-windows)", "ogl3"),
+            ("d3d10pc", "d3d10pc(GPU,2019)",
              "similar to d3d11pc, but for substance 2019"),
             (consts.CUSTOM, "Custom", "Custom")],
         default="$default$",
@@ -60,9 +53,6 @@ class SublenderPreferences(AddonPreferences):
     )
     custom_engine: StringProperty(name="Custom Engine", default='')
     sat_path: StringProperty(name="SAT Installation Path", default='', subtype='DIR_PATH')
-
-    # ("channels", "Follow Channels group",
-    #  "Follow channels group info in graph parameters"),
 
     def draw(self, context):
         layout = self.layout
@@ -78,12 +68,8 @@ class SublenderPreferences(AddonPreferences):
         else:
             row.prop(self,
                      'output_size_y', text='')
-        row = layout.row()
-        row.prop(self, 'compatible_mode',
-                 toggle=1, icon="GHOST_ENABLED")
-        row.prop(self, 'enable_visible_if',
-                 toggle=1, icon="HIDE_OFF")
-        # layout.prop(self, 'update_when_sizing', toggle=1, icon='FILE_REFRESH')
+        layout.prop(self, 'enable_visible_if',
+                    toggle=1, icon="HIDE_OFF")
         layout.prop(self, 'engine_enum')
         if self.engine_enum == consts.CUSTOM:
             layout.prop(self, 'custom_engine')
