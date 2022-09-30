81bfafb11e88d8c814f94301b38b28ccfe3d6ef4
xVanTuring <xVanTuring@outlook.com>
2022-09-30 23:51:55 +0800

    add a build script

---
+++ scripts/build.py
@@ -0,0 +1,16 @@
+import subprocess
+
+result = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+current_version = str(result.stdout, encoding="ascii").strip()
+# git archive --format zip --output E:\sublender.zip master --prefix sublender/
+instr = input("Master(default) or Dev(1):")
+branch = "master"
+if instr == 1:
+    branch = "dev"
+package_name = r"..\sublender_{0}.zip".format(current_version)
+print("Writing {0}".format(package_name))
+subprocess.run([
+    "git", "archive", "--format", "zip",
+    "--output", package_name, branch,
+    "--prefix", "sublender/"
+])
