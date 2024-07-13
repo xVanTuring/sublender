996f69c624ccebd39252636095a56c803f2c8e02
xVanTuring <xVanTuring@foxmail.com>
2024-07-13 13:00:38 +0800

    fix: bash cwd navigation

--- scripts/build_wheel.sh
+++ scripts/build_wheel.sh
@@ -1,7 +1,9 @@
-cd wheels || exit
+pushd wheels || exit
 
 python -m pip wheel ppmd-cffi==0.5.0
 python -m pip wheel bcj-cffi==0.5.1
 python -m pip wheel py7zr==0.20.8
 python -m pip wheel xmltodict==0.13.0
 python -m pip wheel platformdirs==4.1.0
+
+popd || exit
